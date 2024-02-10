82c357413784b6469f048722cd79c82583d34d50
matan003 <sttan002@gmail.com>
2024-02-11 00:57:37 +0100

    Main file

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,26 @@
 #include <vector>
 #include <string>
 
-int main(){
-    std::vector<std::string> msg {"Hello", "World"};
+int main(int argc, char* argv[]){
+    std::string filePath;
 
-    for(const std::string& word : msg){
-        std::cout << word << " ";
+    for(int i = 1; i < argc; i++) {
+        std::string arg = argv[i];
+        if (arg == "-e") {
+            if (i + 1 < argc) {
+                filePath = argv[i++];
+            } else {
+                std::cerr << "-e options requires one argument." << std::endl;
+                return 1;
+            }
+        }
     }
-    std::cout << std::endl;
+
+    if (!filePath.empty()){
+        std::cout << "File path: " << filePath << std::endl;
+    } else {
+        std::cout << "No file path provided." << std::endl;
+    }
+
+    return 0;
 }
